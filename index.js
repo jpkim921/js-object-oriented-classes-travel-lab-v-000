4b1e68263026ad97509fb546a48348ff872a99c6
Jayon Kim <jpkim921@gmail.com>
2018-10-03 01:47:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
 
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+
+}
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let hori_dist = this.endingLocation.horizontal - this.beginningLocation.horizontal
+    let vert_dist = this.endingLocation.vertical - this.beginningLocation.vertical
+    
+  }
+}
